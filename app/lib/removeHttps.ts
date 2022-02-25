12f677fc9e06f2af76f801d76ce4a78b2a24a0a2
lewisblackburn <lewisblackburn10@gmail.com>
2022-02-25 18:20:44 +0000

    feat(removeHttps): deal with http protocol as well as https

--- app/lib/removeHttps.ts
+++ app/lib/removeHttps.ts
@@ -2,6 +2,10 @@ export const removeHttps = (url) => {
   if (url.startsWith("https")) {
     return url.slice(8)
   }
+  if (url.startsWith("http")) {
+    return url.slice(7)
+  }
+
   return url
 }
 
